4d2b2f22e48c54132fe59188a614054e8f870586
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2026-02-08 08:54:42 -0300

    refact: extend html element attributes to InlineError

--- src/components/InlineError/InlineError.tsx
+++ src/components/InlineError/InlineError.tsx
@@ -1,15 +1,20 @@
 import styles from "./inlineError.module.css";
 
-interface InlineErrorProps {
+interface InlineErrorProps extends React.HTMLAttributes<HTMLElement> {
   isShown: boolean;
   label: string;
 }
 
-const InlineError: React.FC<InlineErrorProps> = ({ isShown, label }) => {
+const InlineError: React.FC<InlineErrorProps> = ({
+  isShown,
+  label,
+  ...rest
+}) => {
   return (
     <p
       aria-live="polite"
       className={`${styles.inlineError} ${!isShown && styles.inlineErrorHidden}`}
+      {...rest}
     >
       {label}
     </p>
