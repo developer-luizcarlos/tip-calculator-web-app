56293ce8e8ce589cbac2f9ce1e4d1765f6989514
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2026-02-08 08:05:16 -0300

    refact: add aria-live's value as polite to inlineError

--- src/components/InlineError/InlineError.tsx
+++ src/components/InlineError/InlineError.tsx
@@ -1,18 +1,19 @@
 import styles from "./inlineError.module.css";
 
 interface InlineErrorProps {
-	isShown: boolean;
-	label: string;
+  isShown: boolean;
+  label: string;
 }
 
-const InlineError: React.FC<InlineErrorProps> = ({isShown, label}) => {
-	return (
-		<p
-			className={`${styles.inlineError} ${!isShown && styles.inlineErrorHidden}`}
-		>
-			{label}
-		</p>
-	);
+const InlineError: React.FC<InlineErrorProps> = ({ isShown, label }) => {
+  return (
+    <p
+      aria-live="polite"
+      className={`${styles.inlineError} ${!isShown && styles.inlineErrorHidden}`}
+    >
+      {label}
+    </p>
+  );
 };
 
 export default InlineError;
