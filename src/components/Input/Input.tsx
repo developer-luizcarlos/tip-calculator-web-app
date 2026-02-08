f9564050827c84488db9ba8621e425b44e3b0d69
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2026-02-08 08:02:27 -0300

    refact: add aria-hidden attribute to decorative icon

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,36 +2,32 @@ import styles from "./input.module.css";
 
 import Image from "next/image.js";
 
-import {InputHTMLAttributes, RefObject} from "react";
+import { InputHTMLAttributes, RefObject } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-	hasError: boolean;
-	iconPath?: string;
-	ref?: RefObject<HTMLInputElement | null>;
+  hasError: boolean;
+  iconPath?: string;
+  ref?: RefObject<HTMLInputElement | null>;
 }
 
-const Input: React.FC<InputProps> = ({
-	hasError,
-	iconPath,
-	ref,
-	...rest
-}) => {
-	return (
-		<div
-			className={`${styles.inputWrapper} ${hasError && styles.inputWrapperError}`}
-		>
-			{iconPath && (
-				<Image
-					src={iconPath}
-					alt="icon"
-					height={0}
-					width={0}
-					className={`${styles.icon}`}
-				/>
-			)}
-			<input {...rest} ref={ref} className={`${styles.input}`} />
-		</div>
-	);
+const Input: React.FC<InputProps> = ({ hasError, iconPath, ref, ...rest }) => {
+  return (
+    <div
+      className={`${styles.inputWrapper} ${hasError && styles.inputWrapperError}`}
+    >
+      {iconPath && (
+        <Image
+          src={iconPath}
+          alt="icon"
+          aria-hidden="true"
+          height={0}
+          width={0}
+          className={`${styles.icon}`}
+        />
+      )}
+      <input {...rest} ref={ref} className={`${styles.input}`} />
+    </div>
+  );
 };
 
 export default Input;
